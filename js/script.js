42dee380b9f76f52293a5bbd7ecea8903ede6f21
Alexis Lam <alam8@hawk.iit.edu>
2021-04-13 22:31:30 -0500

    Update script.js
    grabbed element for entryinput

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 function init(){
 //add your javascrip between these two lines of code
 	function myEventFunction(){
-		alert('Hello');
+		alert('Alexis Lam: ' + c1);
 	}
 	
 	var c1 = document.getElementById('entrybutton');
+	var t1 = document.getElementById('entryinput');
 	
 	c1.addEventListener('click', myEventFunction);
 	
 	c1.addEventListener('click', function(){
 		console.log('I was Clicked!');
 	});
+	
+	t1.addEventListener('textcontrol')
+	
 }
 window.addEventListener('load', init);
